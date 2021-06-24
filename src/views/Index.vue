db31718f8f1f9ac667c3a9b03cb8e7cb35ebefe2
孙振宇 <sunzhenyu@live.com>
2021-06-24 15:38:40 +0800

    add vue-router and compress when build

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,18 +1,15 @@
 <template>
-  <div>
-    <router-view />
-  </div>
-
+  <p>index</p>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App',
+  name: 'Home',
 })
 </script>
 
-<style>
+<style scoped>
 
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
+import * as path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(),viteCompression()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
